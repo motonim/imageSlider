b7a2f4ac9cdd9a6a936f16bc56514d57c9ae46ea
Jaeri Park <motonimjrp@gmail.com>
2021-05-11 16:50:30 -0400

    basic setup for js

---
+++ app.js
@@ -0,0 +1,4 @@
+const slider = document.querySelector('.slider');
+const before = document.querySelector('.before');
+const after = document.querySelector('.after');
+const container = document.querySelector('.container');
