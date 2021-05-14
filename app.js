699db864ba70da912f9dca257c63c8fbf9ae6f0a
Jaeri Park <motonimjrp@gmail.com>
2021-05-13 23:45:45 -0400

    updating js

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 const slider = document.querySelector('.slider');
-const before = document.querySelector('.before');
-const after = document.querySelector('.after');
+const before = document.querySelector('.img-container-before');
+const after = document.querySelector('.img-container-after');
 const container = document.querySelector('.container');
+
+const dragbar = (e) => {
+  let xPos = e.layerX;
+  let size = container.offsetWidth;
+
+  before.style.width = xPos + 'px';
+  slider.style.left = xPos + 'px';
+
+  if (xPos < 50) {
+    before.style.width = 0;
+    slider.style.left = 0;
+  } else if (xPos + 50 > size) {
+    before.style.width = size + 'px';
+    slider.style.left = size + 'px';
+  }
+};
+
+container.addEventListener('mousemove', dragbar);
